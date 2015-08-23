0cab8d91ea31508333d06d728e0aa4a05914e08e
Jaime Rodriguez <jrodriguez@giantagency.com>
2015-08-23 08:31:04 -0700

    Added some filters and fixed a bug with trailing slashes

--- urlclass.php
+++ urlclass.php
@@ -15,41 +15,39 @@ namespace Module\URLclass;
  */
 function render() {
 	// Get the current URL
-	$url = $_SERVER['REQUEST_URI'];
+	$url = \Sleepy\Hook::addFilter('urlclass_url', $_SERVER['REQUEST_URI']);
 
 	// Remove the parameters
-	if ($parameters = strlen($url) - (strlen($url) - strpos($url, "?"))) {
+	if ($parameters = strlen($url) - (strlen($url) - strpos($url, '?'))) {
 		$url = substr($url, 0, $parameters);
 	}
 
+	// Remove first slash
+	if (strpos($url, '/') == 0) {
+		$url = substr($url, 1, strlen($url) - 1);
+	}
+
 	// If it doesn't end in php, then add default page
-	if (!strpos($url, ".php")) {
-		$url = $url . "index";
+	if (!strpos($url, '.php')) {
+		// Add trailing slash
+		if (substr($url, -1) !== '/') {
+			$url .= '/';
+		}
+
+		$url = $url . \Sleepy\Hook::addFilter('urlclass_default', 'index');
 	} else {
 		$url = substr($url, 0, strlen($url) - 4);
 	}
 
-	// Remove trailing slash
-	if (strpos($url, "/") == 0) {
-		$url = substr($url, 1, strlen($url) - 1);
-	}
-
-	// Remove the prefix from the class by setting the variable below
-	$prefixes = array(
-		'****DEV_PREFIX****',
-		'****STAGE_PREFIX****',
-		'****LIVE_PREFIX****'
-	);
-
-	foreach ($prefixes as $prefix) {
-		$url = str_replace($prefix, "", str_replace("/", "-", $url));
-	}
-
 	if (empty($url)) {
-		$url = "index";
+		$url = \Sleepy\Hook::addFilter('urlclass_default', 'index');
 	}
 
-	return $url;
+	return \Sleepy\Hook::addFilter('urlclass_class', $url);
 }
 
-\Sleepy\Hook::applyFilter('render_placeholder_urlclass', '\Module\URLclass\render');
+// Apply SM Hooks
+\Sleepy\Hook::applyFilter(
+	'render_placeholder_urlclass',
+	'\Module\URLclass\render'
+);
